72beba047bf84270b7b31b070166e38a5ad675f4
LucasdoPradoTozzi <120129056+LucasdoPradoTozzi@users.noreply.github.com>
2023-10-06 20:47:50 -0300

    começando views fechar pedido

--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import TiposExames
 
-# Create your views here.
+@login_required
+def solicitar_exames(request):
+    tipo_exames = TiposExames.objects.all()
+    if request.method == "GET":
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipo_exames})
+    elif request.method == "POST":
+        exames_id = request.POST.getlist('exames')
+        solicitacao_exames = TiposExames.objects.filter(id__in=exames_id)
+
+        #TODO calcular preço dos exames disponiveis
+        preco_total = 0
+        for i in solicitacao_exames:
+            if i.disponivel:
+                preco_total += i.preco
+
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipo_exames, 'solicitacao_exames': solicitacao_exames, 'preco_total': preco_total})
+
+@login_required
+def fechar_pedido(request):
+    pass
